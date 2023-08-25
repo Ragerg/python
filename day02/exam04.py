d87d8ba8abe301e8ffbcf097d35c102b8c356dd2
Nayeon <in6306174@gmail.com>
2023-08-25 10:58:05 +0900

    약수

--- day02/exam04.py
+++ day02/exam04.py
@@ -17,3 +17,16 @@
     ...
     40의 약수 : [1, 2, 4, 5, 8, 10, 20, 40]
 '''
+
+print('정수를 입력하시오, quit 입력 시 종료됩니다')
+inputs = list()
+while True:
+    data = input()
+    if data.lower() == 'quit':
+        break
+    inputs.append(int(data))
+
+for num in inputs:
+    div = [i+1 for i in range(num) if num % (i+1) == 0]
+    print(f'{num} 의 약수 : {div}')
+
