7dfeffb439955aba3c1dc3de6c6fc605e806167d
Nayeon <in6306174@gmail.com>
2023-08-28 13:27:18 +0900

    최대공약수 구하기 - 교수님 풀이

---
+++ day03/exam07_1.py
@@ -0,0 +1,10 @@
+num, num2 = map(int, input('정수 2개 입력하세요 : ').split())
+print(num, num2)
+
+divNum = [i for i in range(1, num+1) if num % i == 0]
+divNum2 = [i for i in range(1, num2+1) if num2 % i == 0]
+divisor = [n for n in divNum if n in divNum2]
+
+print(divNum)
+print(divNum2)
+print(f'{num}, {num2}의 최대 공약수 : {max(divisor)}')
