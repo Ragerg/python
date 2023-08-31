71faaaeda45cbd21ad40e2b0475136e6e65441fe
Nayeon <in6306174@gmail.com>
2023-08-31 09:15:03 +0900

    클래스_

--- day05/exam08.py
+++ day05/exam08.py
@@ -3,8 +3,10 @@ class Person:
         self.name = name
         self.age = age
         self.__addr = addr # 외부에서 직접적인 접근을 할 수 없다. 비공개
-    def getAddr(self):
+    @property
+    def addr(self):
         return self.__addr
+    @addr.setter
     def setAddr(self, addr):
         self.__addr = addr
     def info(self):
@@ -14,6 +16,6 @@ p = Person('홍길동', 25, '서울')
 p.info()
 print(p.name)
 print(p.age)
-# print(p.addr)
+print(p.addr)
 # print(p.__addr) # error
-print(p.getAddr())
+# print(p.getAddr())
